36ae9c7e5a77e88ad617a129c1142ca502c3a05c
Igor Qroz <igorqueirozdossantoslima_@hotmail.com>
2026-04-26 16:33:30 -0300

    função buscar livros, ok

--- script.js
+++ script.js
@@ -5,6 +5,20 @@ function buscarLivros() {
         .then(resposta => resposta.json())
         .then(dados => {
             console.log("Sucesso! os livros chegaram", dados);
+            const tabela = document.getElementById('tabela-livros')
+            tabela.innerHTML = '';
+            dados.forEach(livro => {
+                tabela.innerHTML += `
+                <tr>
+                        <td>${livro.titulo}</td>
+                        <td>${livro.autor}</td>
+                        <td>
+                            <button>Editar</button>
+                            <button>Excluir</button>
+                        </td>
+                    </tr>
+                `
+            });
         })
         .catch(erro => {
             console.log("Erro ao buscar livros", erro);
