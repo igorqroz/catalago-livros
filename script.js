6b3ff8354060a1032a87c94893643d4a34388904
IgorQroz <igorqueirozdossantoslima_@hotmail.com>
2026-05-06 16:26:33 -0300

    Inicei o processo criar o livro

--- script.js
+++ script.js
@@ -25,3 +25,33 @@ function buscarLivros() {
         })
 }
 buscarLivros()
+
+const formLivro = document.getElementById('form-livro')
+
+formLivro.addEventListener('submit', function (event) {
+    event.preventDefault
+
+    const tituloDigitado = document.getElementById('titulo-livro')
+    const autorDigitado = document.getElementById('nome-autor')
+
+    const novoLivro = {
+        titulo: tituloDigitado,
+        autor: autorDigitado
+    }
+
+    const opcoesDaRequisicao = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(novoLivro)
+    };
+
+    fetch(url, opcoesDaRequisicao)
+        .then(resposta => {
+            if (resposta.ok) {
+                console.log("Livro salvo com sucesso no servidor!");
+            }
+        })
+        .catch(erro => console.error("Erro ao tentar salvar o livro:", erro));
+});
